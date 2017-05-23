0caeef8849c8cdd9a13bdb35b6667073a60ba06b
Joey <joseph.h.ray@gmail.com>
2017-05-24 00:53:56 +1000

    added universal data model

---
+++ PersonalInventory/Inventory.DataModelUniversal/InventoryContext.cs
@@ -0,0 +1,17 @@
+﻿using Inventory.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.DataModel
+{
+    public class InventoryContext : DbContext
+    {
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Possession> Possessions { get; set; }
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=inventory.db");
+        }
+    }
+}
