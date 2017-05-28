2f634063d2849f83dc9e91c18449758f380d7c91
Joey <joseph.h.ray@gmail.com>
2017-05-28 22:40:18 +1000

    bump

--- PersonalInventory/Inenvory.UI/DataModel/InventoryContext.cs
+++ PersonalInventory/Inenvory.UI/DataModel/InventoryContext.cs
@@ -12,7 +12,7 @@ namespace Inventory.DataModel
         public DbSet<History> Histories { get; set; }
         public DbSet<OwnerTodos> OwnerTodos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
+        { 
             optionsBuilder.UseSqlite("Data Source=inventory.db");
         }
     }
